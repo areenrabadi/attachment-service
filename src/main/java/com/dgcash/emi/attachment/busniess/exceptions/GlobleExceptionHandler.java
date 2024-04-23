66e70d582c1051984cf2c3d0b03acee74488f510
AreenAlRabadi <a.rabadi@dg-cash.com>
2024-04-23 13:45:42 +0300

    bugfix/be-improve-attachment-service

--- src/main/java/com/dgcash/emi/attachment/busniess/exceptions/GlobleExceptionHandler.java
+++ src/main/java/com/dgcash/emi/attachment/busniess/exceptions/GlobleExceptionHandler.java
@@ -5,19 +5,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.util.Arrays;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobleExceptionHandler
 {
 	@ExceptionHandler({UploadFileException.class})
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ResponseEntity<String> handleUploadFileException(DigitalCashException ex) {
 		return new ResponseEntity<>(ex.getErrorCode() , HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler({InvalidFileType.class})
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ResponseEntity<String> handleInvalidFileType(InvalidFileType ex) {
 		return new ResponseEntity<>(ex.getErrorCode() , HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler({FileNotFoundOnFTPException.class})
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ResponseEntity<String> handleFileNotFoundOnFTPException(FileNotFoundOnFTPException ex) {
+		return new ResponseEntity<>(ex.getErrorCode() , HttpStatus.NOT_FOUND);
 	}
 }
