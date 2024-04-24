f6771c3026c5f54462c3e93e1536a5f5adfe7e71
AreenAlRabadi <a.rabadi@dg-cash.com>
2024-04-24 15:07:00 +0300

    bugfix/be-improve-cost-service

--- src/main/java/com/dgcash/emi/attachment/busniess/exceptions/GlobleExceptionHandler.java
+++ src/main/java/com/dgcash/emi/attachment/busniess/exceptions/GlobleExceptionHandler.java
@@ -1,9 +1,10 @@
 package com.dgcash.emi.attachment.busniess.exceptions;
 
-import com.dgcash.common.core.data.dtos.translation.ErrorMessageDto;
 import com.dgcash.common.core.exception.DigitalCashException;
 import com.dgcash.emi.attachment.client.translation.TranslationClientHandlerImpl;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -27,25 +28,42 @@ public class GlobleExceptionHandler extends ResponseEntityExceptionHandler {
 	@ExceptionHandler({UploadFileException.class})
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ResponseEntity<String> handleUploadFileException(DigitalCashException ex, WebRequest request) {
-		return new ResponseEntity<>(getErrorMsg(ex, request) , HttpStatus.BAD_REQUEST);
-	}
+		try {
+			return new ResponseEntity<>(getErrorMsg(ex, request), HttpStatus.BAD_REQUEST);
+		}catch (FeignException e){
+			return handleFeignClientException(e);
+		}}
 
 
 	@ExceptionHandler({InvalidFileType.class})
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ResponseEntity<String> handleInvalidFileType(InvalidFileType ex, WebRequest request) {
-		return new ResponseEntity<>(getErrorMsg(ex, request) , HttpStatus.BAD_REQUEST);
-	}
+		try {
+			return new ResponseEntity<>(getErrorMsg(ex, request), HttpStatus.BAD_REQUEST);
+		}catch (FeignException e){
+			return handleFeignClientException(e);
+		}}
 
 	@ExceptionHandler({FileNotFoundOnFTPException.class})
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public ResponseEntity<String> handleFileNotFoundOnFTPException(FileNotFoundOnFTPException ex, WebRequest request) {
-		return new ResponseEntity<>(getErrorMsg(ex, request) , HttpStatus.NOT_FOUND);
+		try {
+			return new ResponseEntity<>(getErrorMsg(ex, request), HttpStatus.NOT_FOUND);
+		} catch (FeignException e){
+			return handleFeignClientException(e);
+	}
 	}
 
+	@ExceptionHandler(FeignException.class)
+	public ResponseEntity<String> handleFeignClientException(FeignException e) {
+		return ResponseEntity.status(e.status()).body(e.contentUTF8());
+	}
+
+	@SneakyThrows(FeignException.class)
 	private String getErrorMsg(DigitalCashException ex, WebRequest request) {
 		String header = Optional.ofNullable(request.getHeader(HttpHeaders.ACCEPT_LANGUAGE))
 				  .filter(StringUtils::isNotBlank)
+				  .map(h -> h.contains("ar") ? "ar" : h.contains("en") ? "en" : "en")
 				  .orElse("en");
 		return translationClientHandler.getErrorMessageByLanguageAndCodeWithParameters(
 				  header, ex.getErrorCode(), ex.getArgs()).getMessage().getPlainText();
