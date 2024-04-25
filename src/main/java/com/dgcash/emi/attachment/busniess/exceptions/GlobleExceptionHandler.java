43249842ba791fd5e44a4fcff719691733a9b3ce
AreenAlRabadi <a.rabadi@dg-cash.com>
2024-04-25 15:02:42 +0300

    bugfix/be-improve-services

--- src/main/java/com/dgcash/emi/attachment/busniess/exceptions/GlobleExceptionHandler.java
+++ src/main/java/com/dgcash/emi/attachment/busniess/exceptions/GlobleExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.dgcash.emi.attachment.busniess.exceptions;
 
+import com.dgcash.common.core.data.dtos.translation.ErrorMessageDto;
 import com.dgcash.common.core.exception.DigitalCashException;
 import com.dgcash.emi.attachment.client.translation.TranslationClientHandlerImpl;
 import feign.FeignException;
@@ -15,7 +16,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Map;
 import java.util.Optional;
 
 @ControllerAdvice
@@ -27,7 +27,7 @@ public class GlobleExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler({UploadFileException.class})
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ResponseEntity<String> handleUploadFileException(DigitalCashException ex, WebRequest request) {
+	public ResponseEntity<?> handleUploadFileException(DigitalCashException ex, WebRequest request) {
 		try {
 			return new ResponseEntity<>(getErrorMsg(ex, request), HttpStatus.BAD_REQUEST);
 		}catch (FeignException e){
@@ -37,7 +37,7 @@ public class GlobleExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler({InvalidFileType.class})
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ResponseEntity<String> handleInvalidFileType(InvalidFileType ex, WebRequest request) {
+	public ResponseEntity<?> handleInvalidFileType(InvalidFileType ex, WebRequest request) {
 		try {
 			return new ResponseEntity<>(getErrorMsg(ex, request), HttpStatus.BAD_REQUEST);
 		}catch (FeignException e){
@@ -46,7 +46,7 @@ public class GlobleExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler({FileNotFoundOnFTPException.class})
 	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public ResponseEntity<String> handleFileNotFoundOnFTPException(FileNotFoundOnFTPException ex, WebRequest request) {
+	public ResponseEntity<?> handleFileNotFoundOnFTPException(FileNotFoundOnFTPException ex, WebRequest request) {
 		try {
 			return new ResponseEntity<>(getErrorMsg(ex, request), HttpStatus.NOT_FOUND);
 		} catch (FeignException e){
@@ -60,13 +60,13 @@ public class GlobleExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 
 	@SneakyThrows(FeignException.class)
-	private String getErrorMsg(DigitalCashException ex, WebRequest request) {
+	private ErrorMessageDto getErrorMsg(DigitalCashException ex, WebRequest request) {
 		String header = Optional.ofNullable(request.getHeader(HttpHeaders.ACCEPT_LANGUAGE))
 				  .filter(StringUtils::isNotBlank)
 				  .map(h -> h.contains("ar") ? "ar" : h.contains("en") ? "en" : "en")
 				  .orElse("en");
 		return translationClientHandler.getErrorMessageByLanguageAndCodeWithParameters(
-				  header, ex.getErrorCode(), ex.getArgs()).getMessage().getPlainText();
+				  header, ex.getErrorCode(), ex.getArgs());
 	}
 
 }
