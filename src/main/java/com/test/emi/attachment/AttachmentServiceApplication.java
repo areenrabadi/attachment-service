3457e3a2279dfb70c443ae72c9c0140cb022f547
areenrabadi <areen.alrabadi@yahoo.com>
2025-03-12 05:22:18 +0300

    Update AttachmentServiceApplication.java

--- src/main/java/com/test/emi/attachment/AttachmentServiceApplication.java
+++ src/main/java/com/test/emi/attachment/AttachmentServiceApplication.java
@@ -8,9 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableDiscoveryClient
-@OpenAPIDefinition(info = @Info(title = "Attachment Service", version = "1.0.0", description = "Attachment Service"))
 public class AttachmentServiceApplication {
 
     public static void main(String[] args) {
